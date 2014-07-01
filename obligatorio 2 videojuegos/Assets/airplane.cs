d9799d9721d3d883e5d0bed9f16e2996c2412b33
Andres Boedo <andresboedo@gmail.com>
2014-07-01 20:57:38 -0300

    se agrega velocidad y controles (XBOX SUPORTED)

---
+++ obligatorio 2 videojuegos/Assets/airplane.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class airplane : MonoBehaviour {
+
+	private float verticalAxis;
+	private float horizontalAxis;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		rigidbody.AddForce (0, 0, 10);
+	}
+
+	void FixedUpdate()
+	{
+		verticalAxis = Input.GetAxis ("Vertical");
+		horizontalAxis = Input.GetAxis ("Horizontal");
+		transform.Rotate (transform.forward * horizontalAxis);
+		transform.Rotate(transform.right * verticalAxis);
+
+		rigidbody.velocity =  transform.up * -10;
+
+	}
+}
--- obligatorio 2 videojuegos/Assets/propeller.cs
+++ obligatorio 2 videojuegos/Assets/propeller.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class plane : MonoBehaviour {
+public class propeller : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
@@ -10,12 +10,11 @@ public class plane : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		transform.Rotate (0, 10, 0);
 	}
 
 	void FixedUpdate()
 	{
 
 	}
-
 }
