3ecd385c52b9c1ba0124b8bd79bd09ce57bf593e
Andres Boedo <andresboedo@gmail.com>
2014-07-01 22:59:37 -0300

    ignorados archivos en temp, agregada main scene

--- obligatorio 2 videojuegos/Assets/airplane.cs
+++ obligatorio 2 videojuegos/Assets/airplane.cs
@@ -13,16 +13,26 @@ public class airplane : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 //		rigidbody.AddForce (0, 0, 10);
+		float fire1Pressed = Input.GetAxis("Fire1");
+
+
+		Debug.Log(fire1Pressed + "");
+		//		float fire2Pressed = Input.GetAxis ("Fire2");
+		rigidbody.AddForce(transform.up * fire1Pressed * -30);
+		//		rigidbody.AddForce(transform.up * fire2Pressed * 30);
+
 	}
 
 	void FixedUpdate()
 	{
 		verticalAxis = Input.GetAxis ("Vertical");
-		horizontalAxis = Input.GetAxis ("Horizontal");
-		transform.Rotate (transform.forward * horizontalAxis);
-		transform.Rotate(transform.right * verticalAxis);
 
-		rigidbody.velocity =  transform.up * -10;
+		horizontalAxis = Input.GetAxis ("Horizontal");
+//		transform.Rotate (transform.forward * horizontalAxis);
+//		transform.Rotate(transform.right * verticalAxis);
+
+
+
 
 	}
 }
