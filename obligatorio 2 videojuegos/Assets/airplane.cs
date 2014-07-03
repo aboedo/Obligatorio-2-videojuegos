fbf3cbb73f6f75b37de59278539e40f4141bec38
Andres Boedo <andresboedo@gmail.com>
2014-07-03 00:22:15 -0300

    mejoras en la fisica y en los controles, ahora todo se maneja en fuerzas y torques

--- obligatorio 2 videojuegos/Assets/airplane.cs
+++ obligatorio 2 videojuegos/Assets/airplane.cs
@@ -6,19 +6,26 @@ public class airplane : MonoBehaviour {
 	private float verticalAxis;
 	private float horizontalAxis;
 	private float maxVelocity = 1000f;
-	private float flapsRotationSpeed = 1f;
+	private float flapsRotationForce = 1000f;
+	private float planeRotationTorque = 10000f;
+	private float minimumFlightSpeed = 20;
 	Animator animator;
 	private bool throttlePressed;
 	private bool flapsLeftPressed;
 	private bool flapsRightPressed;
 	private bool brakePressed;
 	private const float PROPELLER_SPEED_MULTIPLIER = 30f;
-	private const float ENGINE_FORCE = 50f;
+	private const float ENGINE_FORCE = 1000000f;
+	private const float ENGINE_STOP_FORCE = 20000f;
+
+	int propellerMultiplier = 0;
+	private Transform propellerTransform;
 
 	bool grounded = false;
 	// Use this for initialization
 	void Start () {
 		animator = GetComponent<Animator> ();
+		propellerTransform = transform.FindChild ("propeller");
 	}
 	
 	// Update is called once per frame
@@ -32,25 +39,26 @@ public class airplane : MonoBehaviour {
 
 		horizontalAxis = Input.GetAxis ("Horizontal");
 
-		transform.Rotate (new Vector3(1, 0, 0) * verticalAxis);
-		transform.Rotate (new Vector3(0, 1, 0) * horizontalAxis);
-		transform.FindChild("propeller").transform.Rotate (0, (transform.up * rigidbody.velocity.magnitude).z, 0);
-
+		transform.rigidbody.AddTorque (transform.right * verticalAxis * planeRotationTorque);
+		transform.rigidbody.AddTorque (transform.up * horizontalAxis * planeRotationTorque);
 		throttlePressed = Input.GetAxis("Throttle") == 1;
+		if (throttlePressed) {
+			Debug.Log("throttle pressed, add torque to propeller");
+			propellerTransform.rigidbody.AddTorque (propellerTransform.transform.up * PROPELLER_SPEED_MULTIPLIER);
+			Debug.Log (propellerTransform.transform.up * PROPELLER_SPEED_MULTIPLIER);
+		}
+
 		brakePressed = Input.GetAxis("Brake") == 1;
 		flapsLeftPressed = Input.GetAxis("flapsLeft") == 1;
 		flapsRightPressed = Input.GetAxis("flapsRight") == 1;
-		Debug.Log ("right:" + flapsRightPressed);
-		Debug.Log ("left:" + flapsLeftPressed);
 
 		if (flapsLeftPressed && !flapsRightPressed) 
 		{
-
-			transform.Rotate (new Vector3(0, 0, 1) * -flapsRotationSpeed);
+			transform.rigidbody.AddTorque (transform.forward * -flapsRotationForce);
 		}
 		if (flapsRightPressed && !flapsLeftPressed) 
 		{
-			transform.Rotate (new Vector3(0, 0, 1) * flapsRotationSpeed);
+			transform.rigidbody.AddTorque (transform.forward * flapsRotationForce);
 		}
 
 
@@ -58,7 +66,7 @@ public class airplane : MonoBehaviour {
 		if (throttlePressed && rigidbody.velocity.magnitude < maxVelocity) {
 			rigidbody.AddForce(transform.up * -ENGINE_FORCE); 
 		}
-		if (brakePressed && rigidbody.velocity.magnitude > 0) {
+		if (brakePressed && rigidbody.velocity.y > minimumFlightSpeed) {
 			rigidbody.AddForce(transform.up * ENGINE_FORCE); 
 		}
 
