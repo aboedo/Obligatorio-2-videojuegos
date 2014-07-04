8e99bfbdf743783c4a3445e7911f80359393d67b
Andres Boedo <andresboedo@gmail.com>
2014-07-03 23:20:23 -0300

    refactoring del airplane.cs

--- obligatorio 2 videojuegos/Assets/airplane.cs
+++ obligatorio 2 videojuegos/Assets/airplane.cs
@@ -3,55 +3,72 @@ using System.Collections;
 
 public class airplane : MonoBehaviour {
 
-	private float verticalAxis;
-	private float horizontalAxis;
+	#region constants
+
 	private const float MAX_VELOCITY = 1000f;
 	private const float FLAPS_ROTATION_TORQUE = 6000f;
 	private const float PLANE_ROTATION_TORQUE = 10000f;
 	private const float MINIMUM_ROTATION_SPEED = 20f;
-	Animator animator;
+	private const float PROPELLER_SPEED_MULTIPLIER = 30f;
+	private const float ENGINE_THROTTLE_FORCE = 1000000f;
+	private const float ENGINE_THROTTLE_FORCE_GROUNDED = 800000f;
+	private const float ENGINE_STOP_FORCE = 20000f;
+	private const float ENGINE_NORMAL_FORCE = 500000f; 
+
+	#endregion
+
+
+	#region variables
+
+	private Animator animator;
+	private float verticalAxis;
+	private float horizontalAxis;
 	private bool throttlePressed;
 	private bool flapsLeftPressed;
 	private bool flapsRightPressed;
 	private bool brakePressed;
-	private const float PROPELLER_SPEED_MULTIPLIER = 30f;
-	private const float ENGINE_THROTTLE_FORCE = 1000000f;
-	private const float ENGINE_STOP_FORCE = 20000f;
-	private const float NORMAL_ENGINE_FORCE = 500000f; 
-
-	int propellerMultiplier = 0;
+	private int propellerMultiplier = 0;
 	private Transform propellerTransform;
+	private bool grounded = false;
 
-	bool grounded = false;
+	#endregion
+
+	/// ------------------------------------------------------------------------------------------------
 	// Use this for initialization
 	void Start () {
 		animator = GetComponent<Animator> ();
 		propellerTransform = transform.FindChild ("propeller");
 	}
-	
+
+
+	/// ------------------------------------------------------------------------------------------------
 	// Update is called once per frame
 	void Update () {
 
 	}
 
-	void FixedUpdate()
-	{
+
+	/// ------------------------------------------------------------------------------------------------
+	/// <summary>
+	/// Stores all player input in variables
+	/// </summary>
+	void GetAllInput(){
 		verticalAxis = Input.GetAxis ("Vertical");
-
 		horizontalAxis = Input.GetAxis ("Horizontal");
-
-		transform.rigidbody.AddTorque (transform.right * verticalAxis * PLANE_ROTATION_TORQUE);
-		transform.rigidbody.AddTorque (transform.up * horizontalAxis * PLANE_ROTATION_TORQUE);
 		throttlePressed = Input.GetAxis("Throttle") == 1;
 		brakePressed = Input.GetAxis("Brake") == 1;
-		if (!brakePressed && !grounded) {
-			Debug.Log("throttle pressed, add torque to propeller");
-			propellerTransform.rigidbody.AddTorque (propellerTransform.transform.up * PROPELLER_SPEED_MULTIPLIER);
-			Debug.Log (propellerTransform.transform.up * PROPELLER_SPEED_MULTIPLIER);
-		}
-
 		flapsLeftPressed = Input.GetAxis("flapsLeft") == 1;
 		flapsRightPressed = Input.GetAxis("flapsRight") == 1;
+	}
+
+
+	/// ------------------------------------------------------------------------------------------------
+	/// <summary>
+	/// Rotates the plane according to player input
+	/// </summary>
+	void RotatePlaneFromInput(){
+		transform.rigidbody.AddTorque (transform.right * verticalAxis * PLANE_ROTATION_TORQUE);
+		transform.rigidbody.AddTorque (transform.up * horizontalAxis * PLANE_ROTATION_TORQUE);
 
 		if (flapsLeftPressed && !flapsRightPressed) 
 		{
@@ -62,19 +79,63 @@ public class airplane : MonoBehaviour {
 			transform.rigidbody.AddTorque (transform.forward * FLAPS_ROTATION_TORQUE);
 		}
 
+	}
 
 
-		if (throttlePressed && rigidbody.velocity.magnitude < MAX_VELOCITY) {
-			rigidbody.AddForce (transform.up * ENGINE_THROTTLE_FORCE * -1); 
+	/// ------------------------------------------------------------------------------------------------
+	/// <summary>
+	/// Adds torque to the propeller if: 
+	/// - the brake is not pressed while in the air
+	/// - or the plane is on the ground and the throttle is pressed
+	/// </summary>
+	void UpdatePropeller(){
+		if (!brakePressed && (!grounded || (grounded && throttlePressed))) {
+			Debug.Log("throttle pressed, add torque to propeller");
+			propellerTransform.rigidbody.AddTorque (propellerTransform.transform.up * PROPELLER_SPEED_MULTIPLIER);
+			Debug.Log (propellerTransform.transform.up * PROPELLER_SPEED_MULTIPLIER);
+		}
+	}
+
+	/// ------------------------------------------------------------------------------------------------
+	/// <summary>
+	/// Adds force from the engine, according to player input.
+	/// the cases are:
+	/// - if the throttle is pressed while on the ground, apply ENGINE_THROTTLE_FORCE_GROUNDED
+	/// - if the throttle is pressed while on the air, and the speed doesn't exceed max limit, apply ENGINE_THROTTLE_FORCE
+	/// - if the brake is pressed while on the air, and the speed isn't lower than the minimum on the air, apply ENGINE_STOP_FORCE
+	/// - if the plane is on the air and neither the throttle nor the brakes are pressed, apply ENGINE_NORMAL_FORCE.
+	/// </summary>
+	void AddEngineForceFromInput(){
+		if (throttlePressed){
+			if (!grounded && rigidbody.velocity.magnitude < MAX_VELOCITY) {
+				rigidbody.AddForce (transform.up * ENGINE_THROTTLE_FORCE * -1); 
+			}
+			else if (grounded){
+				rigidbody.AddForce (transform.up * ENGINE_THROTTLE_FORCE_GROUNDED * -1); 
+			}
 		} 
 		else if (brakePressed && rigidbody.velocity.y > MINIMUM_ROTATION_SPEED) {
 			rigidbody.AddForce (transform.up * ENGINE_STOP_FORCE * -1); 
 		} 
 		else if (!grounded){
-			rigidbody.AddForce (transform.up * NORMAL_ENGINE_FORCE * -1); 
+			rigidbody.AddForce (transform.up * ENGINE_NORMAL_FORCE * -1); 
 		}
-
 	}
+
+
+	/// ------------------------------------------------------------------------------------------------
+	/// <summary>
+	/// Update the airplane
+	/// </summary>
+	void FixedUpdate()
+	{
+		GetAllInput ();
+		RotatePlaneFromInput ();
+		UpdatePropeller ();
+		AddEngineForceFromInput ();
+	}
+
+
 	void OnCollisionEnter (Collision col)
 	{
 		if(col.gameObject.name == "ground")
