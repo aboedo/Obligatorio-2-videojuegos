e95134685449ede8a443b0f3c530ac03e0230fa5
Andres Boedo <andresboedo@gmail.com>
2014-07-06 18:30:47 -0300

    ahora esta habilitada la camara que mira para atras!! apretar circulo (o boton b, a gusto del fisura)

---
+++ obligatorio 2 videojuegos/Assets/arrows.js
@@ -0,0 +1,27 @@
+﻿var cam : Camera; //Camera to use
+var target : Transform; //Target to point at (you could set this to any gameObject dynamically)
+private var targetPos : Vector3; //Target position on screen
+private var screenMiddle : Vector3; //Middle of the screen
+
+function Update() {
+	//Get the targets position on screen into a Vector3
+	targetPos = cam.WorldToScreenPoint (target.transform.position);
+	//Get the middle of the screen into a Vector3
+	screenMiddle = Vector3(Screen.width/2, Screen.height/2, 0); 
+	//Compute the angle from screenMiddle to targetPos
+	var tarAngle = (Mathf.Atan2(targetPos.x-screenMiddle.x,Screen.height-targetPos.y-screenMiddle.y) * Mathf.Rad2Deg)+90;
+	if (tarAngle < 0) tarAngle +=360;
+	
+	//Calculate the angle from the camera to the target
+	var targetDir = target.transform.position - cam.transform.position;
+	var forward = cam.transform.forward;
+	var angle = Vector3.Angle(targetDir, forward);
+	
+	//If the angle exceeds 90deg inverse the rotation to point correctly
+	if(angle<90){
+		transform.localRotation = Quaternion.Euler(-tarAngle,90,270);
+	} else {
+		transform.localRotation = Quaternion.Euler(tarAngle,270,90);
+	}
+	
+}
---
+++ obligatorio 2 videojuegos/Assets/backCamera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class backCamera : MonoBehaviour {
+	private bool cameraShouldShow = false;
+	// Use this for initialization
+	void Start () {
+		camera.enabled = cameraShouldShow;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonUp ("backCamera")) 
+		{
+			Debug.Log ("show cam");
+			cameraShouldShow = !cameraShouldShow;
+			camera.enabled = cameraShouldShow;
+		}
+
+	}
+}
