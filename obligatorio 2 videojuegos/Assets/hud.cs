66a5975dfa9e06082e853469e5901ac249c99a9a
Andres Boedo <andresboedo@gmail.com>
2014-07-06 01:16:34 -0300

    contador de altutud y otros chiches

--- obligatorio 2 videojuegos/Assets/hud.cs
+++ obligatorio 2 videojuegos/Assets/hud.cs
@@ -3,15 +3,25 @@ using System.Collections;
 
 public class hud : MonoBehaviour {
 
-	Transform speedoMeter;
 	Transform airplane;
-	GUIStyle style;
+	racetrack currentRaceTrack;
+	GUIStyle speedGuiStyle;
+	GUIStyle altitudeGuiStyle;
+	GUIStyle goalsGuiStyle;
+
 
 	// Use this for initialization
 	void Start () {
 		airplane = GameObject.Find ("airplane").transform;
-		style = new GUIStyle();
-		style.fontSize = 36;
+		
+		currentRaceTrack = GameObject.Find ("RaceTrack").GetComponent<racetrack> ();
+		speedGuiStyle = new GUIStyle();
+		speedGuiStyle.fontSize = 36;
+		altitudeGuiStyle = new GUIStyle ();
+		altitudeGuiStyle.fontSize = 24;
+		goalsGuiStyle = new GUIStyle ();
+		goalsGuiStyle.fontSize = 24;
+
 	}
 	
 	// Update is called once per frame
@@ -19,10 +29,38 @@ public class hud : MonoBehaviour {
 	
 	}
 	void OnGUI () {
+		DrawSpeed ();
+		DrawAltitude ();
+		DrawElapsedTime ();
+		DrawCompletedGoals ();
+	}
+
+	void DrawElapsedTime()
+	{
+
+	}
+
+	void DrawCompletedGoals()
+	{
+		int totalGoals = currentRaceTrack.totalGoals;
+		int currentGoal = currentRaceTrack.currentGoal;
+		string completedGoalsString = "Completed: " + currentGoal + "/" + totalGoals;
+		GUI.Box (new Rect ((Screen.width / 2) - 75 , 50, 150, 70), completedGoalsString, goalsGuiStyle);
+	}
+
+	void DrawSpeed()
+	{
 		double speed = airplane.rigidbody.velocity.magnitude;
 		string speedString = Mathf.Round ((float)speed).ToString();
 		string fullSpeedString = speedString + " km/h";
+		GUI.Label (new Rect (Screen.width - 185, Screen.height - 50, 150, 30), fullSpeedString, speedGuiStyle);
+	}
 
-		GUI.Label (new Rect (Screen.width - 175, Screen.height - 50, 150, 30), fullSpeedString, style);
+	void DrawAltitude()
+	{
+		float altitude = airplane.transform.position.y;
+		string altitudeString = Mathf.Round (altitude).ToString();
+		string fullAltitudeString = "Altitud: " + altitudeString + " m";
+		GUI.Box (new Rect (Screen.width - 185, Screen.height - 270, 150, 70), fullAltitudeString, altitudeGuiStyle);
 	}
 }
